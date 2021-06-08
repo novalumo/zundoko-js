320aa5af6ce03155969071bd86b6214e756c7b33
Kento Shirasawa <shirasawa.kento@gmail.com>
2021-06-08 13:13:00 +0900

    KI★YO★SHI

---
+++ README.md
@@ -0,0 +1,3 @@
+# Zundoko JS
+
+Zun, Zun Zun Zundoko KI.YO.SHI on JS
--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('zundoko')
+console.log('------------------------------')
+console.log('ズンドコキヨシ on JavaScript')
+console.log('------------------------------')
+
+// ズンドコ配列
+let zndkAry = ['ズン', 'ドコ']
+let count = 0
+let output = ''
+
+// ズンドコ関数
+const zundoko = (ary) => { 
+
+  while (true) {
+    output = ary[Math.floor(Math.random() * ary.length)]
+    console.log(output)
+    if (count == 4 && output == 'ドコ') {
+      console.log('キ・ヨ・シ！')
+      break
+    } else if (output == 'ズン') {
+      count++
+    } else {
+      count = 0
+    }
+  }
+}
+
+// ズンドコする
+zundoko(zndkAry)
